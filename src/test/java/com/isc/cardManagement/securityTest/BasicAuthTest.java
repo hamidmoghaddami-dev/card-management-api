f7848182dd5177b70884169f8656e27462cf9c64
h.moghadami <h.moghadami@fgostar.com>
2025-12-23 17:22:40 +0330

    some changes applied

--- src/test/java/com/isc/cardManagement/securityTest/BasicAuthTest.java
+++ src/test/java/com/isc/cardManagement/securityTest/BasicAuthTest.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class BasicAuthTest {
+class BasicAuthTest {
 
     @Autowired
     private MockMvc mockMvc;
